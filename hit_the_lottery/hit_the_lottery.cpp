8d49d8d4833bed2a24720f2105e24635ce049237
bliutwo <bliutwo@cs.stanford.edu>
2020-04-01 23:29:34 -0700

    In progress hit_the_lottery

---
+++ hit_the_lottery/hit_the_lottery.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[] = {1, 5, 10, 20, 100};
+    int num_bills = 0;
+    for (int i = 4; i >= 0; i--) {
+        int bill = arr[i];
+        while (n > bill) {
+            n = n - bill;
+            num_bills++;
+        }
+    }
+    cout << num_bills << endl;
+    return 0;
+}
