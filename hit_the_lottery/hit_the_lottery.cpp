594d32eaa73e9c86e08e4517fe241240f47649db
bliutwo <bliutwo@cs.stanford.edu>
2020-04-02 22:18:38 -0700

    In progress hi_the_lottery

--- hit_the_lottery/hit_the_lottery.cpp
+++ hit_the_lottery/hit_the_lottery.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+// #include<map>
 using namespace std;
 
 int main() {
@@ -6,13 +7,18 @@ int main() {
     cin >> n;
     int arr[] = {1, 5, 10, 20, 100};
     int num_bills = 0;
+    cout << "index" << "\t" << "bill" << "\t" << "n";
+    cout << "\t" << "num_bills" << endl;
     for (int i = 4; i >= 0; i--) {
         int bill = arr[i];
         while (n > bill) {
+            cout << i << "\t" << bill << "\t" << n;
+            cout << "\t" << num_bills << endl;
             n = n - bill;
             num_bills++;
         }
     }
+    
     cout << num_bills << endl;
     return 0;
 }
