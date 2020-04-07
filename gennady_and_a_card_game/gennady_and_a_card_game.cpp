8efc50a2d1235f3bacd0b77acbaef46a5b3ddfa3
bliutwo <bliutwo@cs.stanford.edu>
2020-04-06 21:37:25 -0700

    Finish gennady_and_a_card_game

--- gennady_and_a_card_game/gennady_and_a_card_game.cpp
+++ gennady_and_a_card_game/gennady_and_a_card_game.cpp
@@ -3,17 +3,12 @@
 using namespace std;
 
 int main() {
-
-    // TODO: set up ranks and suits such that they only contain ... 
-    //       basically you messed up what ranks and suits should contain
-    //       it should only have stuff from your own cards
-    set<char> ranks = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
-    set<char> suits = {'D', 'C', 'S', 'H'};
-
     string table_card;
     cin >> table_card;
 
     int num_cards_in_hand = 5;
+
+    string can_play = "NO";
 
     for (int i = 0; i < num_cards_in_hand; i++)
     {
@@ -21,14 +16,14 @@ int main() {
         cin >> card_in_hand;
         char rank = card_in_hand[0];
         char suit = card_in_hand[1];
-        if (ranks.find(rank) != ranks.end() or suits.find(suit) != suits.end())
+        if (card_in_hand[0] == table_card[0] or card_in_hand[1] == table_card[1])
         {
-            cout << "YES" << endl;
-            return 0;
+            can_play = "YES";
+            break;
         }
     }
 
-    cout << "NO" << endl;
+    cout << can_play << endl;
     
     return 0;
 }
