ac1973602f1af345e0af7d44d7a5d23ac0140fed
bliutwo <bliutwo@cs.stanford.edu>
2020-04-16 17:27:31 -0700

    Add solution to vus, unconfirmed

---
+++ vus_the_cossack_and_a_contest/vus_the_cossack_and_a_contest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n, m, k;
+    cin >> n >> m >> k;
+    if (n <= m and n <= k)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
