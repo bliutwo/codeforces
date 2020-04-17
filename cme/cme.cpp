9298e6fdc9a4c6a6c21fd71e69605ad063f2fa5a
bliutwo <bliutwo@cs.stanford.edu>
2020-04-16 20:24:28 -0700

    Finish unconfirmed solution

--- cme/cme.cpp
+++ cme/cme.cpp
@@ -13,10 +13,16 @@ int main () {
     for (int i = 0; i < num_queries; i++)
     {
         int num_matches = a[i];
-        int first = num_matches / 2;
-        int sum = first + 1;
-        int total = sum + first + 1;
-        int additional = total - num_matches;
+        int additional;
+        if (num_matches > 2 and num_matches % 2 == 0)
+        {
+            additional = 0;
+        } else {
+            int first = num_matches / 2;
+            int sum = first + 1;
+            int total = sum + first + 1;
+            additional = total - num_matches;
+        }
         a[i] = additional;
     }
     
