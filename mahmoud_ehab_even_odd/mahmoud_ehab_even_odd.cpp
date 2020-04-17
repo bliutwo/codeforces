3c5fd8db08bc755a7b6beb516b4aebfe8b85143f
bliutwo <bliutwo@cs.stanford.edu>
2020-04-16 17:13:41 -0700

    Come up with two solutions for mahmoud_ehab_even_odd

--- mahmoud_ehab_even_odd/mahmoud_ehab_even_odd.cpp
+++ mahmoud_ehab_even_odd/mahmoud_ehab_even_odd.cpp
@@ -10,8 +10,37 @@ int main() {
         if (player == 0) {
             // M's turn, so a has to be even
             // is there a positive even integer 1 <= a <= n?
+            if (n > 1)
+            {
+                // given that, what's the best positive even integer to pick?
+                if (n % 2 == 0) {
+                    // if n is even, just subtract n
+                    n = n - n;
+                } else {
+                    // if n is odd, subtract n - 1
+                    n = n + 1 - n;
+                }
+                player = 1;
+            } else {
+                // can't find any that satisfy the requirements, game ends, M loses
+                break;
+            }
         } else {
             // E's turn, so a has to be odd
+            if (n > 0)
+            {
+                if (n % 2 == 0) {
+                    // if n is even, subtract n - 1
+                    n = n + 1 - n;
+                } else {
+                    // if n is odd, subtract n
+                    n = n - n;
+                }
+                player = 0;
+            } else {
+                // can't find any that satisfy the requirements, game ends, M loses
+                break;
+            }
         }
     }
 
