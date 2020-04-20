dc6c4e7be3011a170f8569d69ce674049bb45fbd
bliutwo <bliutwo@cs.stanford.edu>
2020-04-19 23:44:30 -0700

    Use str.ends_with() but realize C++20 isn't out yet

--- suffix_three/suffix_three.cpp
+++ suffix_three/suffix_three.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <string_view>
 #include <string>
-#include <map>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    map<string, string> suffix_to_language;
-
-    suffix_to_language["po"] = "FILIPINO";
-    suffix_to_language["desu"] = "JAPANESE";
-    suffix_to_language["masu"] = "JAPANESE";
-    suffix_to_language["mnida"] = "KOREAN";
-
     string testcases[n];
 
     for (int i = 0; i < n; i++)
@@ -20,31 +13,15 @@ int main() {
         cin >> testcases[i];
     }
 
-    map<string, string>::iterator it;
     for (int i = 0; i < n; i++)
     {
         string s = testcases[i];
-        string filipino_suffix = s.substr(s.length() - 2, s.length());
-        it = suffix_to_language.find(filipino_suffix);
-        if (it != suffix_to_language.end())
-        {
-            cout << suffix_to_language[filipino_suffix] << endl;
-            continue;
-        }
-        string japanese_suffix = s.substr(s.length() - 4, s.length());
-        it = suffix_to_language.find(japanese_suffix);
-        if (it != suffix_to_language.end())
-        {
-            cout << suffix_to_language[japanese_suffix] << endl;
-            continue;
-        }
-        string korean_suffix = s.substr(s.length() - 5, s.length());
-        it = suffix_to_language.find(korean_suffix);
-        if (it != suffix_to_language.end())
-        {
-            cout << suffix_to_language[korean_suffix] << endl;
-            continue;
-        }
+        if (s.ends_with("po"))
+            cout << "FILIPINO" << endl;
+        else if (s.ends_with("mnida"))
+            cout << "KOREAN" << endl;
+        else
+            cout << "JAPANESE" << endl;
     }
 
     return 0;
