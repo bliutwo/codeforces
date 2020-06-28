846724550132592a13828641a078ea69c8e4dc74
bliutwo <bliutwo@cs.stanford.edu>
2020-06-27 21:22:13 -0700

    27/06/2020 21:22:13

--- chat_room.cc
+++ chat_room.cc
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool said_hello(string s) {
+    string hello = "hello";
+    bool too_soon = false;
+    int curr = 0;
+    for (char c : s) {
+        if (c == hello[curr]) {
+            curr++;
+        } else {
+            if (hello.find(c) != hello.end()) {
+                string before(hello.begin(), hello.begin() + curr);
+            }
+        }
+    }
+    return curr == hello.length();
+}
+
 int main() {
-    // solution here
+    string s;
+    cin >> s;
+    if (said_hello(s)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
     return 0;
 }
