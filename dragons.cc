fc7d3880328d670009c6d890e92636a2594471db
bliutwo <bliutwo@cs.stanford.edu>
2021-01-11 02:48:46 -0800

    Complete dragons https://codeforces.com/problemset/problem/230/A

--- dragons.cc
+++ dragons.cc
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct dragon {
+    int x;
+    int y;
+};
+
+struct less_than_key
+{
+    inline bool operator() (const dragon& struct1, const dragon& struct2)
+    {
+        return (struct1.x < struct2.x);
+    }
+};
+
 int main() {
     // solution here
     int s, n;
     cin >> s >> n;
-    int x[n];
-    int y[n];
-    int indices[n];
+    vector<dragon> dragons;
+
     for (int i = 0; i < n; ++i) {
-        cin >> x[i] >> y[i];
-        indices[i] = i;
+        dragon d;
+        cin >> d.x >> d.y;
+        dragons.push_back(d);
     }
-    bool success = false;
-    do {
-        // do something
-        for (int i = 0; i < n; i++) {
-            int index = indices[i];
-            if (x[index] > s) {
-                break;
-            } else {
-                s += y[index];
-                if (i == n - 1) {
-                    success = true;
-                }
-            }
-        }
-        if (success)
+
+    sort(dragons.begin(), dragons.end(), less_than_key());
+
+    bool success = true;
+
+    for (size_t i = 0; i < dragons.size(); i++) {
+        dragon d = dragons[i];
+        if (s > d.x) {
+            s += d.y;
+        } else {
+            success = false;
             break;
-    } while(next_permutation(indices, indices + n));
+        }
+    }
+
     if (success) {
         cout << "YES" << endl;
     } else {
