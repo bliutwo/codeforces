888b1b6bb627eca4f8af6056f1b735141337eb1d
bliutwo <bliutwo@cs.stanford.edu>
2021-03-11 23:53:07 -0800

    Add skeleton death_note

---
+++ death_note.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // solution here
+    int n, m;
+    cin >> n >> m;
+    int numNames[n];
+    for (int i = 0; i < n; i++) {
+        cin >> numNames[i];
+    }
+    return 0;
+}
