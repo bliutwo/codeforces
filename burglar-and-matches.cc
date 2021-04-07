6981957e888007067a14cae3997ca9aa6acb5dc8
bliutwo <bliutwo@cs.stanford.edu>
2021-04-06 20:52:03 -0700

    Start solution

---
+++ burglar-and-matches.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int num_matchboxes[m];
+    int num_matches[m];
+
+    for (int i = 0; i < m; i++) {
+        cin >> num_matchboxes[i] >> num_matches[i];
+    }
+
+    return 0;
+}
