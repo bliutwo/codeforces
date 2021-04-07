c9ecb1b615049f79b96f4f69cddbad7815d91817
bliutwo <bliutwo@cs.stanford.edu>
2021-04-06 22:09:34 -0700

    Add solution in progress

--- burglar-and-matches.cc
+++ burglar-and-matches.cc
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int num_matchboxes[m];
-    int num_matches[m];
+    vector<pair<int, int>> matchbox_and_matches;
 
     for (int i = 0; i < m; i++) {
-        cin >> num_matchboxes[i] >> num_matches[i];
+
     }
 
     return 0;
