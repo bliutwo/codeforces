bda37319dd5f7508138aa3a16c854148733ae92a
bliutwo <bliutwo@cs.stanford.edu>
2021-04-09 01:16:06 -0700

    Sort vector by second element in pair, descending

--- burglar-and-matches.cc
+++ burglar-and-matches.cc
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool sort_by_second_in_pair(pair<int, int> a, pair<int, int> b) {
+    return a.second > b.second;
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
@@ -11,6 +15,9 @@ int main() {
         cin >> p.first >> p.second;
         matchbox_and_matches.push_back(p);
     }
+
+    // sort vector descending (big to small) by second element in pair
+    sort(matchbox_and_matches.begin(), matchbox_and_matches.end(), sort_by_second_in_pair);
 
     for (int i = 0; i < m; i++) {
         pair<int, int> p = matchbox_and_matches[i];
