88982303e424921d3e510798bfd78b43a9c4e438
bliutwo <bliutwo@cs.stanford.edu>
2021-04-09 01:09:15 -0700

    Get pairs of integers

--- burglar-and-matches.cc
+++ burglar-and-matches.cc
@@ -7,7 +7,14 @@ int main() {
     vector<pair<int, int>> matchbox_and_matches;
 
     for (int i = 0; i < m; i++) {
+        pair<int, int> p;
+        cin >> p.first >> p.second;
+        matchbox_and_matches.push_back(p);
+    }
 
+    for (int i = 0; i < m; i++) {
+        pair<int, int> p = matchbox_and_matches[i];
+        cout << p.first << " " << p.second << endl;
     }
 
     return 0;
