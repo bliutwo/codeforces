d713fc757d24ec18a8cecc06204c0b8bc6b38761
bliutwo <bliutwo@cs.stanford.edu>
2021-04-28 21:34:27 -0700

    Add solution in progress

---
+++ sum-of-2050.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long nums[n];
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    return 0;
+}
