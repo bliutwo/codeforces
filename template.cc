6c1056d87211d5065325ba12fc4b0a6155832b06
bliutwo <bliutwo@cs.stanford.edu>
2021-04-30 02:57:36 -0700

    Add some optimizations

--- template.cc
+++ template.cc
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Make input and output more efficient.
+ios::sync_with_stdio(0);
+cin.tie(0);
+
 int main() {
     // solution here
+    // Newline character is faster because endl always causes a flush operation.
+    cout << "Hello world!\n";
     return 0;
 }
