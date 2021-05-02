579b635d3e099bdd41ff696b042df815cbee68ad
bliutwo <bliutwo@cs.stanford.edu>
2021-05-01 19:28:48 -0700

    Add optimizations in the correct spot in program

--- template.cc
+++ template.cc
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Make input and output more efficient.
-ios::sync_with_stdio(0);
-cin.tie(0);
+
 
 int main() {
-    // solution here
+    // Make input and output more efficient.
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     // Newline character is faster because endl always causes a flush operation.
     cout << "Hello world!\n";
+    // solution here
     return 0;
 }
