a94e34c4e4c24feac499a47f0215aea6b0718c36
bliutwo <bliutwo@cs.stanford.edu>
2021-08-08 14:02:55 -0700

    Revert "Add nonworking solution"
    This reverts commit 0f910afcd216ea298066a45cca5713f72a01a48f.

--- pizzaforces.cc
+++ pizzaforces.cc
@@ -6,9 +6,13 @@ using namespace std;
 void solve(int n, vector<int>& slices, vector<int>& times,
            priority_queue <int, vector<int>, greater<int>>& pq,
            int totalTime) {
+    if (n == 0) {
+        pq.push(totalTime);
+        return;
+    }
     for (int i = 0; i < slices.size(); i++) {
-        int remainder = n % slices[i];
-        if (remainder == 0) {
+        int remainder = n - slices[i];
+        if (remainder < 0) {
             pq.push(totalTime + times[i]);
         } else {
             solve(remainder, slices, times, pq, totalTime + times[i]);
