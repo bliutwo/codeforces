7da40e47134b2dbc55b46305d08808960e2dc964
bliutwo <bliutwo@cs.stanford.edu>
2021-09-06 23:04:27 -0700

    Start solution

---
+++ scenes_from_memory.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(int k, int n) {
+}
+
+int main() {
+    // Make input and output more efficient.
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // Newline character is faster because endl always causes a flush operation.
+    // cout << "Hello world!\n";
+    // solution here
+    int t;
+    cin >> t;
+    while (t--) {
+        int k, n;
+        cin >> k >> n;
+        solve(k, n);
+    }
+    return 0;
+}
